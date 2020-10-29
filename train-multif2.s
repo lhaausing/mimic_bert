6b3dffdf82b5d4a59df2d5618e2699d9852b0e38
Gaomin Wu <gw1107@nyu.edu>
2020-10-29 11:30:39 -0400

    add longformer pretrain

---
+++ train-multif2.s
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=1
+#SBATCH --gres=gpu:p40:1
+#SBATCH --time=24:00:00
+#SBATCH --mem=100GB
+#SBATCH --job-name=multi_file_train
+#SBATCH --mail-type=END
+#SBATCH --mail-user=gw1107@nyu.edu
+#SBATCH --output=/scratch/gw1107/capstone/output_log/test_multifiles2.log
+ 
+source activate NYU_DL 
+python3 2_finetune_on_pretrained_longformer-mem2.py
